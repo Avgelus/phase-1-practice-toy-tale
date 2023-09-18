99f3bda7be7490a957185ac29942f7fb0c3bf7ee
johnnyhall <jfh2345@yahoo.com>
2023-09-18 12:59:24 -0400

    entire lab

--- src/index.js
+++ src/index.js
@@ -13,3 +13,43 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+fetch('http://localhost:3000/toys')
+.then((resp) => resp.json())
+.then((data) => renderToys(data))
+
+function renderToys (toyArray) {
+  toyArray.forEach(uploadToy)
+} 
+
+function uploadToy(toyObject){
+  const pageDiv = document.querySelector('#toy-collection')
+  const spaceToys = document.createElement('div') 
+  const toyName = document.createElement('h2') 
+  const toyImage = document.createElement('img')
+  const peeTag = document.createElement('p') 
+  const toyButton = document.createElement('button')
+
+  toyName.textContent = toyObject.name
+  toyImage.src = toyObject.image 
+  peeTag.textContent = toyObject.likes +  ' likes' 
+  
+   
+  toyButton.id = '[toy_id]'
+  toyButton.textContent = "Like" 
+  toyButton.className = "[like-btn]"
+  spaceToys.className = 'card' 
+  toyImage.className ='toy-avatar'
+  
+  
+  
+  spaceToys.setAttribute('card', toyImage)
+  spaceToys.append(toyName, toyImage, peeTag,toyButton)
+  pageDiv.append(spaceToys) 
+
+
+
+
+
+  
+} 
+
